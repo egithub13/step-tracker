75d3561952488201e1ddf2c6e003f71bc8b2caf6
Eric Hayes <egithub13@gmail.com>
2024-08-10 22:18:55 -0400

    created xcode project

---
+++ StepTracker/StepTracker/StepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerApp.swift
+//  StepTracker
+//
+//  Created by eric hayes on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
